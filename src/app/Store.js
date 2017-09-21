f5a638fa443ea9552a629aa5e9589ee6ff9ebd30
Siyan Panayotov <xsisqox@gmail.com>
2017-09-21 22:34:31 +0200

    Extract Section class out of Store.js

--- src/app/Store.js
+++ src/app/Store.js
@@ -4,6 +4,7 @@ import BaseObject from './BaseObject'
 import {Drawable, Container} from './Drawables'
 import Tiles from './Tiles'
 import Color from './Color'
+import Section from './Section'
 import game from './Game'
 
 export default class Store {
@@ -189,83 +190,5 @@ export default class Store {
         }
 
         return this.sections
-    }
-}
-
-class Section extends BaseObject {
-    constructor() {
-        super()
-
-        this.x = 0;
-        this.y = 0;
-        this.w = 0;
-        this.h = 0;
-        this.color = '#dad'
-    }
-
-    get area() {
-        return this.w * this.h
-    }
-
-    get xMax () {
-        return this.x + this.w
-    }
-
-    get yMax () {
-        return this.y + this.h
-    }
-
-    getTiles () {
-        if ( this.hasOwnProperty('tiles') ) {
-            return this.tiles;
-        }
-
-        this.tiles = new Tiles()
-        this.tiles.resize(this.w, this.h)
-
-        let vertical = this.w > this.h
-        let padding = Math.round(game.prngs.pcg.next())
-        let step = Math.round(game.prngs.pcg.next()) + 2
-
-        // Remove random padding for odd lengths
-        if ( vertical && ((this.w%2) == 1) ) {
-            padding = 0;
-        } else if ( ! vertical && ((this.h%2) == 1) ) {
-            padding = 0;
-        }
-
-        for (let x = this.x; x < this.xMax; x++) {
-            for (let y = this.y; y < this.yMax; y++) {
-                let draw = vertical ? (this.xMax - x + padding) % step: (this.yMax - y + padding) % step;
-
-                if (draw) {
-                    this.tiles.set(x - this.x, y - this.y, 1)
-                }
-            }
-        }
-
-        return this.tiles
-    }
-
-    divide () {
-        if ( this.area < 24 ) return false;
-
-        let props = this.w > this.h ? ['x', 'w']: ['y', 'h'];
-        let cut = Math.round(this[props[1]] * (0.5 + (game.prngs.pcg.next()-0.5) * 0.2))
-
-        let sibling = new Section().set({
-            x: this.x,
-            y: this.y,
-            w: this.w,
-            h: this.h
-        })
-
-        sibling.color = game.prngs.pcg.color();
-        sibling[props[0]] += cut
-        sibling[props[1]] -= cut
-
-        this[props[1]] = cut - 1;
-
-        return sibling
     }
 }
