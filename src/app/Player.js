ec86fbcd7e5a0b3dd795d2a69141ab3ea635ca16
Siyan Panayotov <siyan@hellosiyan.com>
2017-12-03 22:57:55 +0100

    Separate player movement and drawable ghost

--- src/app/Player.js
+++ src/app/Player.js
@@ -1,52 +1,29 @@
-import Rect from './lib/Rect';
+import Container from './lib/Container';
+import ChildGhost from './elements/ChildGhost';
 import game from './Game';
 
-export default class Player extends Rect {
+export default class Player extends Container {
     constructor() {
         super();
+
+        this.visible = false;
 
         this.direction = {
             x: '', // [ud]
             y: '', // [lr]
         };
 
+        this.ghost = new ChildGhost();
+
+        this.addChild(this.ghost);
+
         this.width = game.config.size.me;
-        let drawHeight = this.width / 20 * 26;
-        this.drawHeight = Math.ceil(drawHeight);
-        this.height = Math.ceil(drawHeight * 0.1);
-
-        this.x = game.config.size.grid;
-        this.y = game.config.size.grid;
-
-        this.levitationTimeInterval = 0;
+        this.height = Math.ceil(Math.ceil(game.config.size.me / 20 * 26) * 0.1);
 
         this.speed = {
             x: 0,
             y: 0,
         };
-    }
-
-    draw(ctx) {
-        ctx.mozImageSmoothingEnabled = false;
-        ctx.webkitImageSmoothingEnabled = false;
-        ctx.msImageSmoothingEnabled = false;
-        ctx.imageSmoothingEnabled = false;
-
-        let spriteName = this.direction.y == 'u' ? 'back' : 'front';
-
-        this.levitationTimeInterval = (this.levitationTimeInterval + game.loop.dt) % 2;
-        let levitationHeightRatio = Math.abs(this.levitationTimeInterval - 1);
-        let levitationY = Math.round(levitationHeightRatio * this.drawHeight * 0.2);
-
-        let x = this.x;
-        let y = this.y - Math.round(this.drawHeight - this.height) - levitationY;
-
-        if (this.direction.x == 'r') {
-            ctx.scale(-1, 1);
-            x = -1 * this.x - this.width;
-        }
-
-        game.spritesheets.ghost.draw(spriteName, ctx, x, y, this.width, this.drawHeight);
     }
 
     move() {
@@ -93,5 +70,7 @@ export default class Player extends Rect {
                 this.y += this.speed.y * game.loop.dt;
             }
         }
+
+        this.ghost.direction = this.direction;
     }
 }
---
+++ src/app/elements/ChildGhost.js
@@ -0,0 +1,41 @@
+import Drawable from '../lib/Drawable';
+import game from '../Game';
+
+export default class ChildGhost extends Drawable {
+    constructor() {
+        super();
+
+        this.width = game.config.size.me;
+        this.height = Math.ceil(game.config.size.me / 20 * 26);
+
+        this.levitationTimeInterval = 0;
+
+        this.direction = {
+            x: '', // [ud]
+            y: '', // [lr]
+        };
+    }
+
+    draw(ctx) {
+        ctx.mozImageSmoothingEnabled = false;
+        ctx.webkitImageSmoothingEnabled = false;
+        ctx.msImageSmoothingEnabled = false;
+        ctx.imageSmoothingEnabled = false;
+
+        const spriteName = this.direction.y == 'u' ? 'back' : 'front';
+
+        this.levitationTimeInterval = (this.levitationTimeInterval + game.loop.dt) % 2;
+        const levitationHeightRatio = Math.abs(this.levitationTimeInterval - 1) * 0.2;
+        const levitationY = Math.round(levitationHeightRatio * this.height);
+
+        let x = this.x;
+        let y = this.y - this.height - levitationY;
+
+        if (this.direction.x == 'r') {
+            ctx.scale(-1, 1);
+            x = -1 * this.x - this.width;
+        }
+
+        game.spritesheets.ghost.draw(spriteName, ctx, x, y, this.width, this.height);
+    }
+}
