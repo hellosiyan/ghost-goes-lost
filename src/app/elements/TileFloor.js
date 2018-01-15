9b5a1f1c1eff2838fc0e2db5c8079717985f63aa
Siyan Panayotov <siyan@hellosiyan.com>
2018-01-15 19:01:08 +0100

    Define floor tile graphic through Pixmap

--- src/app/elements/TileFloor.js
+++ src/app/elements/TileFloor.js
@@ -1,54 +1,37 @@
 import Drawable from '../lib/Drawable';
+import Pixmap from '../lib/Pixmap';
+
+let colorKey = {
+    '0': '#132339',
+    '1': '#162c44',
+    '2': '#17314a',
+};
+
+const pixmap = Pixmap.load(`
+000000000000002
+011111111111112
+011111111111112
+011111111111112
+011111111111112
+011111111111112
+011111111111112
+011111111111112
+011111111111112
+011111111111112
+011111111111112
+011111111111112
+011111111111112
+011111111111112
+222222222222222`, colorKey);
 
 export default class TileFloor extends Drawable {
-    constructor() {
-        super();
-
-        this.tilePadding = 1;
-        this.tileWidth = 10;
-        this.tileHeight = 10;
-        this.tile = document.createElement('canvas');
-    }
-
     draw(ctx) {
-        this.drawTile();
-
-        ctx.fillStyle = ctx.createPattern(this.tile, 'repeat');
-        ctx.globalAlpha = this.style.opacity;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-
-    drawTile() {
-        let ctx = this.tile.getContext('2d');
-
-        this.tile.width = this.tileWidth;
-        this.tile.height = this.tileHeight;
-
-        // Base color
-        ctx.fillStyle = this.style.color.copy().lighten(0.1).toString();
-        ctx.fillRect(
-            0,
-            0,
-            this.tileWidth,
-            this.tileHeight
-        );
-
-        // Padded base
-        ctx.fillStyle = this.style.color.copy().darken(0.25).toString();
-        ctx.fillRect(
-            this.tilePadding,
-            this.tilePadding,
-            this.tileWidth,
-            this.tileHeight
-        );
-
-        // 2nd padded base
-        ctx.fillStyle = this.style.color.copy().toString();
-        ctx.fillRect(
-            this.tilePadding * 2,
-            this.tilePadding * 2,
-            this.tileWidth - this.tilePadding * 2,
-            this.tileHeight - this.tilePadding * 2
-        );
+        pixmap
+            .toPatternedDrawable({ height: this.height, width: this.width }, 'repeat')
+            .set({
+                x: this.x,
+                y: this.y
+            })
+            .draw(ctx);
     }
 }
