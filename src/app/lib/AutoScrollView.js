52ec92a7dcfb4bca150877d2afe5c2fdb7a7dd26
Siyan Panayotov <siyan@hellosiyan.com>
2018-01-31 22:14:07 +0100

    Fix AutoScrollView disregarding the target's dimensions

--- src/app/lib/AutoScrollView.js
+++ src/app/lib/AutoScrollView.js
@@ -28,18 +28,28 @@ export default class AutoScrollView extends Container {
     }
 
     moveTargetIntoView() {
-        let coords = this.target.positionAtAncestor(this);
+        const coords = this.target.positionAtAncestor(this);
 
-        if (coords.x < this.boundries.left) {
+        if (this.target.width < this.boundries.right - this.boundries.left ) {
+            if (coords.x < this.boundries.left) {
+                this.children[0].x += this.boundries.left - coords.x;
+            } else if (coords.x + this.target.width > this.boundries.right) {
+                this.children[0].x -= coords.x + this.target.width - this.boundries.right;
+            }
+        } else {
+            // Target is wider than allowed zone - default to left align
             this.children[0].x += this.boundries.left - coords.x;
-        } else if (coords.x > this.boundries.right) {
-            this.children[0].x -= coords.x - this.boundries.right;
         }
 
-        if (coords.y < this.boundries.top) {
+        if (this.target.height < this.boundries.bottom - this.boundries.top ) {
+            if (coords.y < this.boundries.top) {
+                this.children[0].y += this.boundries.top - coords.y;
+            } else if (coords.y + this.target.height > this.boundries.bottom) {
+                this.children[0].y -= coords.y + this.target.height - this.boundries.bottom;
+            }
+        } else {
+            // Target is higher than allowed zone - default to top align
             this.children[0].y += this.boundries.top - coords.y;
-        } else if (coords.y > this.boundries.bottom) {
-            this.children[0].y -= coords.y - this.boundries.bottom;
         }
     }
 }
