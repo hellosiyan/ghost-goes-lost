0811d0851e8fb30474f3c7fa00a5a4c236aafb45
Siyan Panayotov <siyan@hellosiyan.com>
2018-07-28 17:53:20 +0200

    Add default and watch actions

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,34 @@ const  inlinesource = require('gulp-inline-source');
 const rename = require('gulp-rename');
 
 gulp.task('build', () => {
+    return do_rollup('./src/index.js', './dist/library.js')
+        .then(() => do_closure_compile('./dist/library.js', './dist/'))
+        .then(() => do_clean_css('./src/styles/main.css', 'dist'))
+        .then(() => do_inline_source('./src/index.template.html', './dist'))
+        .then(() => postbuild());
+});
+
+gulp.task('default', () => {
+    return do_rollup('./src/index.js', './dist/library.js')
+        .then(() => do_clean_css('./src/styles/main.css', 'dist'));
+});
+
+gulp.task('build_source', () => {
+    return do_rollup('./src/index.js', './dist/library.js');
+});
+
+gulp.task('build_styles', () => {
+    return do_clean_css('./src/styles/main.css', 'dist');
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.js', ['build_source']);
+    gulp.watch('src/styles/main.css', ['build_styles']);
+});
+
+function do_rollup(source, destination) {
     return rollup.rollup({
-        input: './src/index.js',
+        input: source,
         plugins: [
             resolve(),
             commonjs()
@@ -19,58 +45,56 @@ gulp.task('build', () => {
     })
     .then(bundle => {
         return bundle.write({
-            file: './dist/library.js',
+            file: destination,
             format: 'iife',
             // name: 'library',
             // sourcemap: true
         });
-    })
-    .then(() => {
-        return new Promise(function(resolve, reject) {
-            gulp.src('./dist/library.js', {base: './dist/'})
-                .pipe(compiler({
-                    // compilationLevel: 'ADVANCED',
-                    compilationLevel: 'SIMPLE',
-                    // compilationLevel: 'WHITESPACE_ONLY',
-                    warningLevel: 'QUIET',
-                    // outputWrapper: '(function(){\n%output%\n}).call(this)',
-                    jsOutputFile: 'library.cjs.js',  // outputs single file
-                    languageIn: 'ECMASCRIPT6',
-                    languageOut: 'ECMASCRIPT6',
-                    rewritePolyfills: false,
-                    // assumeFunctionWrapper: true,
-                    // createSourceMap: true,
-                }))
-                .pipe(gulp.dest('./dist'))
-                .on('error', reject)
-                .on('end', resolve)
-        })
-    })
-    .then(() => {
-        return new Promise(function(resolve, reject) {
-            gulp.src('./src/styles/main.css')
-                .pipe(cleanCSS())
-                .pipe(gulp.dest('dist'))
-                .on('error', reject)
-                .on('end', resolve)
-        })
-    })
-    .then(() => {
-        return new Promise(function(resolve, reject) {
-            gulp.src('./dist/index.template.html')
-                .pipe(inlinesource({
-                    compress: false,
-                }))
-                .pipe(rename('index.html'))
-                .pipe(gulp.dest('./dist'))
-                .on('error', reject)
-                .on('end', resolve)
-        })
-    })
-    .then(() => {
-        return postbuild();
     });
-});
+}
+
+function do_closure_compile(source, destination) {
+    return new Promise(function(resolve, reject) {
+        gulp.src(source, {base: destination})
+            .pipe(compiler({
+                // compilationLevel: 'ADVANCED',
+                compilationLevel: 'SIMPLE',
+                // compilationLevel: 'WHITESPACE_ONLY',
+                warningLevel: 'QUIET',
+                // outputWrapper: '(function(){\n%output%\n}).call(this)',
+                jsOutputFile: 'library.cjs.js',  // outputs single file
+                languageIn: 'ECMASCRIPT6',
+                languageOut: 'ECMASCRIPT6',
+                rewritePolyfills: false,
+                // assumeFunctionWrapper: true,
+                // createSourceMap: true,
+            }))
+            .pipe(gulp.dest(destination))
+            .on('error', reject)
+            .on('end', resolve)
+    })
+}
+
+function do_clean_css(source, destination) {
+    return new Promise(function(resolve, reject) {
+        gulp.src(source)
+            .pipe(cleanCSS())
+            .pipe(gulp.dest(destination))
+            .on('error', reject)
+            .on('end', resolve)
+    })
+}
+
+function do_inline_source(source, destination) {
+    return new Promise(function(resolve, reject) {
+        gulp.src(source)
+            .pipe(inlinesource({compress: false}))
+            .pipe(rename('index.html'))
+            .pipe(gulp.dest(destination))
+            .on('error', reject)
+            .on('end', resolve)
+    })
+}
 
 function postbuild() {
     // fs.unlinkSync('./dist/script.js')
